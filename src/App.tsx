892ef8df42356128f919727aa707c23280c8b8a7
Caleb Rector <calebrector77@gmail.com>
2024-11-25 20:25:01 -0800

    Add wakatime bar graph

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,22 @@ import {
 } from '@chakra-ui/react';
 import Spinner from './components/custom/spinner';
 import CenterBox from './components/custom/centerbox';
+import { Cell, Tooltip, BarChart, XAxis, Bar } from 'recharts';
 //import * as wakatime from './utils.js';
 
 async function getAllTimeSinceToday() {
-	const response = await fetch('https://wakatime.com/share/@59402cab-1799-4733-b30d-ec5d8ed0a7c9/96f3a5ba-466c-4297-aa5e-195fbec31b37.json');
+	const response = await fetch('https://wakatime.com/share/@59402cab-1799-4733-b30d-ec5d8ed0a7c9/69dd4ca1-3a95-49d1-910b-85cab2183232.json');
 	return response.json();
 }
 
-const data = await getAllTimeSinceToday();
-console.log(data);
+const white = '#E5E5E5';
+const yellow = '#FD972B';
+const orange = '#DD4624';
+const purple = '#E5E5E5';
+const chartColors = [yellow, orange, purple]
+
+const res = await getAllTimeSinceToday();
+const langData = res.data.slice(0, 5);
 
 function App() {
 	return (
@@ -45,9 +52,17 @@ function App() {
 					Vite, C++, Java, HTML, CSS, Jira, Git, GitHub, JSON, Electron, and more
 				</p>
 			</CenterBox>
-			<CenterBox title="Lookit this" dotted>
-				
-
+			<CenterBox title="Work breakdown (past week)" dotted>
+				<BarChart width={500} height={300} data={langData}>
+					<XAxis dataKey="name" />
+					<Tooltip />
+					<Bar dataKey="decimal" fill={chartColors[0]}>
+						{langData.map((entry: any, index: any) => (
+							<Cell key={`cell-${index}`} fill={chartColors[index%chartColors.length]} />
+						))}
+					</Bar>
+				</BarChart>
+				<p>Powered by <a href="https://wakatime.com/dashboard">WakaTime</a></p>
 			</CenterBox>
 		</>
 	);
